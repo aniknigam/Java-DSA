e3f327e8a9021c2c8d95af4d9f3ab891db71eebe
Aniket Nigam <aniknigam2@gmail.com>
2023-07-03 15:36:39 +0530

    2d array problem

--- array2d.java
+++ array2d.java
@@ -33,6 +33,6 @@ public class array2d {
             }
             System.out.println();
         }
-
+        sc.nextInt();
     }
 }
--- array2dP2.java
+++ array2dP2.java
@@ -39,5 +39,6 @@ public class array2dP2 {
             }
         }
       }
+      sc.nextInt();
     }
 }
