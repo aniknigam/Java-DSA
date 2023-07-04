e22796dc10a59eeed43db6722b9680822af13c86
Aniket Nigam <aniknigam2@gmail.com>
2023-07-05 02:13:49 +0530

    strings

---
+++ Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String[] args) {
+        // string declaration
+        String name = "Aniket ";
+        String fullName = "Aniket Nigam ";
+        String sentence = " My full name is Aniket Nigam ";
+
+        System.out.println(name+fullName+sentence);
+    }
+}
