dfae2be8b1d6a831d580e17717415d4cefa62f40
Aniket Nigam <aniknigam2@gmail.com>
2023-07-13 01:10:04 +0530

    bobble sort

---
+++ bubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class bubbleSort {
+
+    public static int printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 1, 5, 2, 8, 7 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
