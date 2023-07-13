1b09e40b3e9d2fcdf99233720c9d5238a5f166e4
Aniket Nigam <aniknigam2@gmail.com>
2023-07-13 16:16:53 +0530

    insertion sort

---
+++ Sorting/insertionSort.java
@@ -0,0 +1,20 @@
+public class insertionSort {
+    public static void main(String[] args) {
+        int[] arr = { 8, 1, 4, 5, 3, 9 };
+
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
