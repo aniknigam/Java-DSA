2bfa546f489b70d54d38c416d02091ab12ca521c
Aniket Nigam <aniknigam2@gmail.com>
2023-07-26 00:11:01 +0530

    recursion

---
+++ advRecursion/advanceP5.java
@@ -0,0 +1,38 @@
+//print all the subset of the set of n natural numbers
+
+package advRecursion;
+
+import java.util.ArrayList;
+
+public class advanceP5 {
+
+    public static void printSubset(ArrayList<Integer> subset) {
+        for (int i = -0; i < subset.size(); i++) {
+            System.out.print(subset.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubset(int n, ArrayList<Integer> subset) {
+        // base case
+        if (n == 0) {
+            printSubset(subset);
+            return;
+        }
+
+        // will add
+        subset.add(n);
+        findSubset(n - 1, subset);
+
+        // will not add
+        subset.remove(subset.size() - 1);
+        findSubset(n - 1, subset);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubset(n, subset);
+    }
+}
