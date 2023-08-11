ef15df0168b069731566a0f1d82446e33e8fe835
Aniket Nigam <aniknigam2@gmail.com>
2023-08-12 00:16:55 +0530

    diameter of a tree

---
+++ Tree/diameterOfTreeA2.java
@@ -0,0 +1,71 @@
+package Tree;
+
+public class diameterOfTreeA2 {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        int index = -1;
+
+        public Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        static class treeInfo {
+            int ht;
+            int dia;
+
+            treeInfo(int ht, int dia) {
+                this.ht = ht;
+                this.dia = dia;
+            }
+        }
+
+        public treeInfo diameter(Node root) {
+            if (root == null) {
+                return new treeInfo(0, 0);
+            }
+
+            // takeout the height
+            treeInfo left = diameter(root.left);
+            treeInfo right = diameter(root.right);
+
+            int myheight = Math.max(left.ht, right.ht) + 1; // +1 is the root node addition
+
+            int diam1 = left.dia;
+            int diam2 = right.dia;
+            int diam3 = left.ht + right.ht + 1;
+
+            int mydiameter = Math.max(Math.max(diam1, diam2), diam3);
+
+            treeInfo myInfo = new treeInfo(myheight, mydiameter);
+            return myInfo;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(tree.diameter(root).dia);
+
+    }
+}
