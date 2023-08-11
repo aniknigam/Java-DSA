9855d4a67377172850db50444430a347f6bbcf8d
Aniket Nigam <aniknigam2@gmail.com>
2023-08-11 11:46:31 +0530

    Math max() method

---
+++ simple java/mathmax.java
@@ -0,0 +1,9 @@
+public class mathmax {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int c = Math.max(a, b);
+        System.out.println("maximum number is: " + c);
+    }
+}
