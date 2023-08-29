9e9919a8b6089e73828e8ca4b0fe3b29c3695abe
Aniket Nigam <aniknigam2@gmail.com>
2023-08-29 17:31:03 +0530

    Backtracking

---
+++ Backtracking/allcombo.java
@@ -0,0 +1,23 @@
+//WAP to print all the combination of the string "ABC"
+package Backtracking;
+
+public class allcombo {
+
+    public static void printcombo(String str, String comb, int idx) {
+        if (str.length() == 0) {
+            System.out.println(comb);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);// We are not including the ith char in this
+            // newStr from 0 to i doesn't include i and i+1 doesn't inclue i
+            printcombo(newStr, comb + currChar, idx + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "ABC";
+        printcombo(str, "", 0);
+    }
+}
